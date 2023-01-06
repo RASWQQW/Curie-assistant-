8ffff2882b49ddf88ca9632ac8668908e5c23969
Rasul <94111424+RASWQQW@users.noreply.github.com>
2023-01-06 21:53:52 +0600

    v3

---
+++ Curyassistant/tools/Classes/Sup.py
@@ -0,0 +1,21 @@
+# from multiprocessing import Process
+# import CHOICEassistance.Curyassistant.speaker.Curie as Molly
+#
+# class Runner:
+#     def __init__(self, Dict):
+#         from CHOICEassistance.Curyassistant.speaker.Curie import IterForWiki
+#         self.dict = Dict
+#
+#     def TalkLike(self):
+#         print(self.dict)
+#         Molly.speak(next(self.dict['list']))
+#
+#
+# class LongActionFlow(Process):
+#     def __init__(self, la_object):
+#         self.la = la_object
+#         Process.__init__(self)
+#
+#     def run(self):
+#         self.la.TalkLike()
+
