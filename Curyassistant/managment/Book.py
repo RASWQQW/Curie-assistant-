3752433c11fa9b71d3a80c1e877a410eca8bcf21
Rasul <94111424+RASWQQW@users.noreply.github.com>
2023-01-06 21:54:28 +0600

    v4

---
+++ Curyassistant/managment/Book.py
@@ -0,0 +1,12 @@
+import asyncio
+import json
+from bot.apps.ownProjects.BookFinder.FinderWithGoogle import GetOurs
+
+def Finder(authorName: str = None, BookName: str = None):
+    a = GetOurs()
+    elem = asyncio.run(a.Waiter(authorName, BookName, user_id=43452242, get_last=True, link=True))
+    # del a.contentList; print("Yes Compiled")
+    print(json.dumps(elem, indent=4, separators=(',', ': '), ensure_ascii=False))
+    return elem
+
+# Finder("Atomic Habits", "James Clear")
